10ce5a969d084bc49514bf382b15db007c9de263
soponalways <soponalways@gmail.com>
2025-08-14 00:01:05 +0600

    integreate google fonts

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div data-theme="dark">
+    <div data-theme="dark" className='merienda-font'>
       <HelmetProvider>
         <RouterProvider router={router} />
       </HelmetProvider>
